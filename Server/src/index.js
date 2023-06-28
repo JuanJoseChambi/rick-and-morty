e8a9a3d6ebf43147a249a5b8c4ff729fad97894d
juanjose <chambijuanjose05@gmail.com>
2023-06-28 17:24:29 -0300

    database changes

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,9 @@
-const server = require("./app")
+const server = require("./app");
 const PORT = 3001;
+const { conn } = require("./DB_connection");
 
 server.listen(PORT, () => {
+  conn.sync({ force: false });
   console.log("Server raised in port: " + PORT);
 });
 
